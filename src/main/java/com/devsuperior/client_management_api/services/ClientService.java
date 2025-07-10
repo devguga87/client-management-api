5f0179795a463fa402b7157aba5f67de6cbae828
Gustavo Seabra <devguga87@gmail.com>
2025-07-10 13:33:37 -0300

    feat: adiciona funcionalidade de buscar todos os clientes de forma paginada

--- src/main/java/com/devsuperior/client_management_api/services/ClientService.java
+++ src/main/java/com/devsuperior/client_management_api/services/ClientService.java
@@ -4,6 +4,8 @@ import com.devsuperior.client_management_api.dto.ClientDTO;
 import com.devsuperior.client_management_api.entities.Client;
 import com.devsuperior.client_management_api.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,11 @@ public class ClientService {
         Optional<Client> result = repository.findById(id);
         Client client = result.get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
